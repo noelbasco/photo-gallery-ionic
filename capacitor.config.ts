ddccc239c016060b5dfc2cb957a0bab301d7e808
noelbasco <oel_0319@yahoo.com>
2024-03-06 12:37:51 +0800

    rev 1

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,12 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.company.appname',
-  appName: 'My Capacitor App',
-  webDir: 'www',
   plugins: {
     LiveUpdates: {
-      appId: '042a1261',
+      appId: '72fee20c',
       channel: 'Production',
       autoUpdateMethod: 'background',
       maxVersions: 2
